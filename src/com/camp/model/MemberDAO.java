dc7a886c0872c5c7fb2fcbbd4020e3d0d331ea9e
woojin <artwoojin@gmail.com>
2025-04-22 19:09:55 +0900

    [Feat] 비밀번호 수정

--- src/com/camp/model/MemberDAO.java
+++ src/com/camp/model/MemberDAO.java
@@ -1,5 +1,6 @@
 package com.camp.model;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
@@ -61,6 +62,29 @@ public class MemberDAO {
 			session.rollback();
 			e.printStackTrace();
 		} finally {
+			session.close();
+		}
+		return result;
+	}
+	
+	// 비밀번호 수정
+	public boolean updatePw(String memberId, String pw, String newPw){
+		SqlSession session = DBCP.getSqlSessionFactory().openSession();
+		boolean result = false;
+		HashMap<String, String> map = new HashMap<>();
+		map.put("memberId", memberId);
+		map.put("pw", pw);
+		map.put("newPw", newPw);
+		try{
+			int i = session.update("memberMapper.updatePw", map);
+			if (i == 1){
+				session.commit();
+				result = true;
+			}
+		}catch(Exception e){
+			session.rollback();
+			e.printStackTrace();
+		}finally{
 			session.close();
 		}
 		return result;
