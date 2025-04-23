cc7d133cc9644dacd8800e7fb60e2d5fa7b923bc
YunhoYeom <whatever.u.r1103@gmail.com>
2025-04-23 15:55:40 +0900

    [feat] Service - CommentsDAO&Test 예시

---
+++ src/com/camp/model/CommentsDAO.java
@@ -0,0 +1,29 @@
+package com.camp.model;
+
+import java.util.List;
+import org.apache.ibatis.session.SqlSession;
+
+public class CommentsDAO {
+
+    // 댓글 목록 조회
+    public List<CommentsVO> getDetailComments(SqlSession session, int postId) {
+        return session.selectList("commentsMapper.getComments", postId);
+    }
+
+    // 댓글 등록
+    public boolean addComments(SqlSession session, CommentsVO vo) {
+        boolean result = false;
+        try {
+            session.insert("commentsMapper.addComments", vo);
+            result = true;
+        } catch (Exception e) {
+            
+        }
+        return result;
+    }
+
+    // 댓글 작성자의 프로필 이미지 가져오기
+    public String getMemberImage(SqlSession session, String memberId) {
+        return session.selectOne("commentsMapper.getMemberImage", memberId);
+    }
+}
