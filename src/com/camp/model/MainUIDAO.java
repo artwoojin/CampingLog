137b8b5c3f154b47b369b1981663a620892a2731
glodtwo1989 <ttpd19891213@gmail.com>
2025-04-23 16:59:23 +0900

    [init] mainUIDAO 추가

---
+++ src/com/camp/model/MainUIDAO.java
@@ -0,0 +1,31 @@
+package com.camp.model;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+public class MainUIDAO {
+	/* 인기 상승 캠핑장 상위 3개*/
+	public Collection<PostVO> getPopularCampingList(int categoryId) {
+		SqlSession conn = DBCP.getSQLSqlSessionFactory().openSession();
+		List<PostVO> list = conn.selectList("CampingLogMapper.getPopularCampingList", categoryId);
+		conn.close();
+		return list;
+	}
+	//상위3개 상세조회
+	
+	
+	
+	/* 명예의 캠핑로거 상위 5개*/
+	public Collection<MemberVO> getTopMembersByLikes(int i) {
+	    SqlSession conn = DBCP.getSQLSqlSessionFactory().openSession();
+	    List<MemberVO> List = null;
+	    List = conn.selectList("CampingLogMapper.getTopMemberRank", 5);
+	    conn.close();
+	    return List;
+	}
+	
+	//상위5명 프로필 상세조회
+
+}
