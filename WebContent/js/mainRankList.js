95da9e7f38887e7f21224102d1ea2dfd7bbe18fe
glodtwo1989 <ttpd19891213@gmail.com>
2025-04-24 19:58:09 +0900

    [feat] mainRankUpAction 부분 js 추가

--- WebContent/js/mainRankList.js
+++ WebContent/js/mainRankList.js
@@ -3,28 +3,40 @@ $(document).ready(function () {
 	$.ajax({
 		url: "controller",
 		method: "GET",
-		data: { cmd: "memberRankListAction" },
-		success: function (responseText) {
-			const data = typeof responseText === "string" ? JSON.parse(responseText) : responseText;
+		data: { cmd: "mainRankUpAction" },
+		success: function (response) {
+			console.log("전체 회원 등급 갱신 완료", response.result);
 
-			let html = "<div class='rankList'><div class='rankDate'><p>2025.04 기준</p></div>";
+			$.ajax({
+				url: "controller",
+				method: "GET",
+				data: { cmd: "memberRankListAction" },
+				success: function (responseText) {
+					const data = typeof responseText === "string" ? JSON.parse(responseText) : responseText;
 
-			$.each(data, function (index, member) {
-				html += ''
-					+ '<div class="rankCard">'
-					+ '  <div class="rankBadge"><img src="img/' + member.badgeImage + '" alt="' + (index + 1) + '등 뱃지"></div>'
-					+ '  <div class="rankInfo">'
-					+ '    <p class="rankDetail">' + (index + 1) + '등</p>'
-					+ '    <p class="rankNickname">' + member.nickName + '</p>'
-					+ '  </div>'
-					+ '</div>';
+					let html = "<div class='rankList'><div class='rankDate'><p>2025.04 기준</p></div>";
+
+					$.each(data, function (index, member) {
+						html += ''
+							+ '<div class="rankCard">'
+							+ '  <div class="rankBadge"><img src="img/' + member.badgeImage + '" alt="' + (index + 1) + '등 뱃지"></div>'
+							+ '  <div class="rankInfo">'
+							+ '    <p class="rankDetail">' + (index + 1) + '등</p>'
+							+ '    <p class="rankNickname">' + member.nickName + '</p>'
+							+ '  </div>'
+							+ '</div>';
+					});
+
+					html += "</div>";
+					$(".downRank").html(html);
+				},
+				error: function () {
+					console.error("명예의 캠핑로거 데이터 불러오기 실패");
+				}
 			});
-
-			html += "</div>";
-			$(".downRank").html(html);
 		},
 		error: function () {
-			console.error("명예의 캠핑로거 데이터 불러오기 실패");
+			console.error("전체 회원 등급 갱신 실패");
 		}
 	});
 });
