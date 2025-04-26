6ac400ace6002cbb65f96a9283bedf60ef41b7d9
glodtwo1989 <ttpd19891213@gmail.com>
2025-04-26 22:57:50 +0900

    [feat] yesterdayRankListAction 추가

--- WebContent/js/mainRankList.js
+++ WebContent/js/mainRankList.js
@@ -10,13 +10,17 @@ $(document).ready(function () {
 			$.ajax({
 				url: "controller",
 				method: "GET",
-				data: { cmd: "memberRankListAction" },
+				data: { cmd: "yesterdayRankListAction" },
 				success: function (responseText) {
 					const data = typeof responseText === "string" ? JSON.parse(responseText) : responseText;
 
-					let html = "<div class='rankList'><div class='rankDate'><p>2025.04 기준</p></div>";
+					let html = "<div class='rankList'>";
+                    // rankDate도 같이 받아와서 표시
+                    const rankDate = data.rankDate || "기준일 없음";
+                    html += "<div class='rankDate'><p>" + rankDate + " 기준</p></div>";					
+					
 
-					$.each(data, function (index, member) {
+					$.each(data.rankList, function (index, member) {
 						html += ''
 							+ '<div class="rankCard">'
 							+ '  <div class="rankBadge"><img src="img/' + member.badgeImage + '" alt="' + (index + 1) + '등 뱃지"></div>'
