ec1f5bd4ed33a36ab98d37fa689d4e8ef8ec0855
jaewonn02 <leejw2669@gmail.com>
2025-04-26 09:50:27 +0900

    [chore] "헤더 카테고리아이디 Action 추가"

---
+++ src/com/camp/servlet/GetHeaderCategoryIdAction.java
@@ -0,0 +1,22 @@
+package com.camp.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import com.camp.model.PostDAO;
+
+public class GetHeaderCategoryIdAction implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request) throws ServletException, IOException {
+		
+		int categoryId = Integer.parseInt(request.getParameter("categoryId"));
+		
+		System.out.println(categoryId);
+		
+		PostDAO dao = new PostDAO();
+		return "header.jsp";
+	}
+}
