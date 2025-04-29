a4826984493a4ad1d90b4e35d165c8b736b5ca4b
jaewonn02 <leejw2669@gmail.com>
2025-04-29 16:19:34 +0900

    [chore] "GetPostsByPageAction 수정"

--- src/com/camp/servlet/GetPostsByPageAction.java
+++ src/com/camp/servlet/GetPostsByPageAction.java
@@ -17,27 +17,29 @@ public class GetPostsByPageAction implements Action {
 	@Override
 	public String execute(HttpServletRequest request) throws ServletException, IOException {
 		// 파라미터 받기
-        int page = Integer.parseInt(request.getParameter("page"));
-        int categoryId = Integer.parseInt(request.getParameter("categoryId"));
-        int pageSize = 4;
-        System.out.println(categoryId);
+		int page = Integer.parseInt(request.getParameter("page"));
+		int categoryId = Integer.parseInt(request.getParameter("categoryId"));
+		String tagList = request.getParameter("tagList");      // ex) "무료,반려동물"
+		if (tagList == null) tagList = ""; 
+		int pageSize = 4;
 
-        int start = (page - 1) * pageSize + 1;
-        int end = page * pageSize;
-        
-        System.out.println(start + " " + end);
+		int start = (page - 1) * pageSize + 1;
+		int end = page * pageSize;
 
+		System.out.println(categoryId);
+		System.out.println(tagList);
+		System.out.println(start + " " + end);
 
-        PostDAO dao = new PostDAO();
-        List<PostVO> posts = dao.getPostPageByCategory(categoryId, start, end);
-        int totalPosts = dao.getTotalPostCountByCategory(categoryId);
+		PostDAO dao = new PostDAO();
+		List<PostVO> posts = dao.getPostPageByCategory(categoryId, tagList , start, end);
+		int totalPosts = dao.getTotalPostCountByCategory(categoryId, tagList);
 
-        request.setAttribute("postList", posts);
-        request.setAttribute("totalPostCount", totalPosts);
-        request.setAttribute("currentPage", page);
+		System.out.println(posts);
 
-        return "postData.jsp"; // 
-    }
+		request.setAttribute("postList", posts);
+		request.setAttribute("totalPostCount", totalPosts);
+		request.setAttribute("currentPage", page);
 
-
+		return "postData.jsp"; // 
+	}
 }
