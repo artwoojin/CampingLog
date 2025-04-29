08d8330b52a2fe870208f2d16ed63d1c0bac6c8b
jaewonn02 <leejw2669@gmail.com>
2025-04-29 16:18:38 +0900

    [chore] "SearchResultAction 수정"

--- src/com/camp/servlet/SearchResultAction.java
+++ src/com/camp/servlet/SearchResultAction.java
@@ -1,20 +1,40 @@
 package com.camp.servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
-public class SearchResultAction implements Action {
+import com.camp.model.PostDAO;
+import com.camp.model.PostVO;
 
-   @Override
-   public String execute(HttpServletRequest request) throws ServletException, IOException {
-      String page="searchResult.html";
-      //처리 데이터를 추출
-      String searchTerm=request.getParameter("searchTerm");
-      
-      
-      
-      return page;
-   }
+public class SearchResultAction implements Action {
+	@Override
+	public String execute(HttpServletRequest request) throws ServletException, IOException {
+		String searchTerm=request.getParameter("searchTerm");
+		System.out.println(searchTerm);
+		// 파라미터 받기
+		int page = Integer.parseInt(request.getParameter("page"));
+		int pageSize = 4;
+
+
+		int start = (page - 1) * pageSize + 1;
+		int end = page * pageSize;
+
+		System.out.println(start + " " + end);
+
+
+		PostDAO dao = new PostDAO();
+		List<PostVO> posts = dao.getPostPage(start, end, searchTerm);
+		int totalPosts = dao.getTotalPostCount(searchTerm);
+
+		System.out.println(posts);
+
+		request.setAttribute("postList", posts);
+		request.setAttribute("totalPostCount", totalPosts);
+		request.setAttribute("currentPage", page);
+
+		return "searchTermData.jsp"; // 
+	}
 }
