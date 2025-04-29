0b8502dc831807cc10bf2c3480c2f16763e05121
jaewonn02 <leejw2669@gmail.com>
2025-04-29 15:51:32 +0900

    [chore] "headerCategory.js 수정"

--- WebContent/js/headerCategory.js
+++ WebContent/js/headerCategory.js
@@ -46,58 +46,5 @@ $(document).ready(function () {
       });
   }
 
-  function loadLoginMemberInfo() {
-    // 초기 상태로 미리 설정 (깜빡임 방지)
-    $("#loginBox").html(`<span style="color:gray; font-size:12px;">로그인 상태 확인 중...</span>`);
-
-    $.ajax({
-      url: "controller",
-      method: "GET",
-      data: { cmd: "getMyInfo" },
-      dataType: "json",
-      success: function (data) {
-        if (!data || !data.memberId) {
-          showLoginUI();
-          return;
-        }
-
-        const profileImg = data.memberImage && data.memberImage !== "null" ? data.memberImage : "defaultProfile.png";
-        const html = `
-          <div class="logoutButton">
-            <button id="logoutBtn">로그아웃</button>
-          </div>
-          <div class="profilePhoto">
-            <a href="myPage.html">
-              <img src="upload/${profileImg}" alt="프로필 이미지" class="profile-img" title="${data.nickName}" />
-            </a>
-          </div>
-        `;
-
-        $("#loginBox").html(html);
-
-        $("#logoutBtn").on("click", function () {
-          location.href = "controller?cmd=logout";
-        });
-      },
-      error: function () {
-        showLoginUI();
-      }
-    });
-
-    function showLoginUI() {
-      const html = `
-        <div class="authButton">
-          <button onclick="location.href='signUp.html'">회원가입</button>
-        </div>
-        <div class="loginButton">
-          <button onclick="location.href='login.html'">로그인</button>
-        </div>
-      `;
-      $("#loginBox").html(html);
-    }
-  }
-
-  // 호출 순서
   loadHeaderWithNames();
-  loadLoginMemberInfo();
 });
